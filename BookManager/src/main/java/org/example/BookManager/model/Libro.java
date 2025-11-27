cfd20a1ec115fcee8abd7830423303c14ad44191
Diego David Fonseca Moody <166345558+Diego-Fonseca29@users.noreply.github.com>
2025-11-27 10:08:35 -0600

    Avance de BookManager
    pantallas de usuario, prestamo  y libro funcional

---
+++ BookManager/src/main/java/org/example/BookManager/model/Libro.java
@@ -0,0 +1,43 @@
+package org.example.BookManager.model;
+
+import javax.persistence.*;
+import org.openxava.annotations.*;
+
+@Entity
+public class Libro {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Hidden
+    private Long id;
+
+    @Column(length = 100) @Required
+    private String titulo;
+
+    @Column(length = 100)
+    private String autor;
+
+    @Column(length = 50)
+    private String isbn;
+
+    @Column(length = 500) @TextArea
+    private String descripcion;
+
+    private Integer añoPublicacion;
+
+    private Integer cantidadEjemplares;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public Integer getAñoPublicacion() { return añoPublicacion; }
+    public void setAñoPublicacion(Integer añoPublicacion) { this.añoPublicacion = añoPublicacion; }
+    public Integer getCantidadEjemplares() { return cantidadEjemplares; }
+    public void setCantidadEjemplares(Integer cantidadEjemplares) { this.cantidadEjemplares = cantidadEjemplares; }
+}
